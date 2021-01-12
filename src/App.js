18fa291e5807017128eb31ceaa5f4a60557d0570
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-01-12 11:47:46 +0000

    Set up the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     //BEM
     <div className="app">
-      <h1>Hello React, please be nice</h1>
+      <Header />
+      {/*Home*/}
     </div>
   );
 }
