dde952477e5c4bfb302cf9b4e9f2b0a4e3f43cbb
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-01-15 20:54:33 +0000

    Make the basket count in header works

--- src/Product.js
+++ src/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Product.css";
+import {useStateValue} from "./StateProvider";
 
 function Product({id, title, image, price, rating}) {
   const [{basket}, dispatch] = useStateValue();
