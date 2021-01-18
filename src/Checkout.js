18464da06f3b401f7107eda7383d0764b19e7bdc
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-01-18 15:55:19 +0000

    Update feature in the checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -12,7 +12,7 @@ function Checkout() {
         <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
 
         <div>
-          <h3>Hello, {user?.email}</h3>
+          <h3>Hello, {!user ? 'Guest' : user.email}</h3>
           <h2 className="checkout__title">Your shopping Basket</h2>
           {basket.map(item => (
             <CheckoutProduct 
