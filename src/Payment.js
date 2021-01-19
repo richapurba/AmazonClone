4f6c8f86422e6e4382829e55caf3095f9b1855b6
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-01-19 06:39:13 +0000

    Update payment.js

--- src/Payment.js
+++ src/Payment.js
@@ -4,6 +4,7 @@ import {useStateValue} from "./StateProvider";
 
 function Payment() {
   const [{basket, user}, dispatch] = useStateValue();
+
   return (
     <div className="payment">
       <div className="payment__container">
@@ -13,13 +14,20 @@ function Payment() {
           </div>
           <div className="payment__address">
             <p>{user?.email}</p>
+            <p>123 Mountainview Lane</p>
+            <p>Pasadena, CA</p>
           </div>
         </div>
-        <div className="payment__section"></div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items"></div>
+        </div>
         <div className="payment__section"></div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Payment;
