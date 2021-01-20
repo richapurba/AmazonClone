26cf95911eb17234b7f6dca7b7bb751e331f3d57
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-01-20 17:54:32 +0000

    Add stripe API for the payment process

--- src/Payment.js
+++ src/Payment.js
@@ -1,30 +1,72 @@
 import React from "react";
 import "./Payment.css";
 import {useStateValue} from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import {Link} from "react-router-dom";
+import {CardElement, useStripe, useElements} from "@stripe/react-stripe-js";
 
 function Payment() {
   const [{basket, user}, dispatch] = useStateValue();
 
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+
+  const handleSubmit = e => {
+
+  }
+
+  const handleChange = e => {
+
+  }
+
   return (
     <div className="payment">
       <div className="payment__container">
+        <h1>Checkout (<Link to="/checkout">{basket?.length} items</Link>)</h1>
         <div className="payment__section">
           <div className="payment__title">
             <h3>Delivery Address</h3>
           </div>
           <div className="payment__address">
             <p>{user?.email}</p>
-            <p>123 Mountainview Lane</p>
+            <p>123 Mountain view Lakes</p>
             <p>Pasadena, CA</p>
           </div>
         </div>
+
         <div className="payment__section">
           <div className="payment__title">
-            <h3>Review items and delivery</h3>
-          </div>
-          <div className="payment__items"></div>
+            <h3>Review items an delivery</h3>
+           </div>
         </div>
-        <div className="payment__section"></div>
+
+        <div className="payment__section">
+          <div className="payment__items">
+            {basket.map(item => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.title}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange}/>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
